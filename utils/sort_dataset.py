d20e2d7f189529b786380e479184ecc3750a5e2f
Geoforger <brayshaw.george@gmail.com>
2024-07-10 15:34:48 +0100

    Edited for bigdataset and depths

--- utils/sort_dataset.py
+++ utils/sort_dataset.py
@@ -3,31 +3,47 @@ import os
 import pandas as pd
 
 def main():
-    dataset_path = "/media/farscope2/T7 Shield/Neuromorphic Data/Ao/0618_Slide_allspeed/"
+    dataset_path = "/media/farscope2/T7 Shield/Neuromorphic Data/Ao/bigdataset/"
     output_path = "/media/farscope2/T7 Shield/Neuromorphic Data/George/all_speeds_sorted/"
     try:
         os.mkdir(output_path)
     except:
         print("Directory exists already")
 
+    # NOTE:
+    # Data is currently saved in format:
+    # ".bigdataset/TEXTUREA_TEXTUREB/...depth_X_speed_Y/events/...trial_Z_pose_T_events_on"
+
     # Find all textures within this path
-    dir_list = os.listdir(dataset_path)
-    textures = sum([d.split("_") for d in dir_list],[])
+    tex_dirs = os.listdir(dataset_path)
+    textures = sum([d.split("_") for d in tex_dirs],[])
     num_textures = len(textures)
 
     # Find all speeds within this texture
     # NOTE: Assumes same speeds are collected for each
-    speed_dirs = os.listdir(dataset_path+dir_list[0])
-    speeds = [s.split("_")[-1] for s in speed_dirs]
+    speed_dirs = os.listdir(dataset_path+tex_dirs[0])
+    speeds = list(set([s.split("_")[-1] for s in speed_dirs])) # Set to list to remove duplicates
+    speeds = sorted([int(s) for s in speeds])   # Sort these in accending speed - requires conversion to int
+    speeds = [str(s) for s in speeds]
     num_speeds = len(speeds)
+
+    # Find all depths
+    depth_dirs = os.listdir(dataset_path + tex_dirs[0])
+    depths = list(set([d.split("_")[-3] for d in depth_dirs]))
+    num_depths = len(depths)
+    set_depth = 1.5
 
     print("Textures:")
     print(textures)
     print("Speeds:")
-    print(speeds)    
+    print(speeds)   
+    print("Depths:")
+    print(depths)
+    print(f"Set Depth: {set_depth}mm")
 
+    # NOTE: This only takes data from set depth
     # Copy files into sorted folder
-    for d in dir_list:
+    for d in tex_dirs:
         # Find speed directories for these textures
         speed_d = os.listdir(dataset_path + d)
 
@@ -39,22 +55,24 @@ def main():
 
             # Set speed of each of these samples
             speed = s.split("_")[-1]
+            depth = s.split("_")[-3]
 
-            for f in files:
-                # Textures are the directory name as pose 1 and 2
-                # Eg. MESH_FELT: pose_0 = MESH, pose_1 = FELT
+            if depth == str(set_depth):
+                for f in files:
+                    # Textures are the directory name as pose 1 and 2
+                    # Eg. MESH_FELT: pose_0 = MESH, pose_1 = FELT
 
-                split_name = f.split("_")
-                trial = split_name[-5]
-                pose = int(split_name[-3])
+                    split_name = f.split("_")
+                    trial = split_name[-5]
+                    pose = int(split_name[-3])
 
-                # Find texture and its position in the overall texture list
-                tex = dir_textures[pose]
-                texture = textures.index(tex)
+                    # Find texture and its position in the overall texture list
+                    tex = dir_textures[pose]
+                    texture = textures.index(tex)
 
-                # Copy file into sorted folder with new name
-                output_name = f"{trial}-{texture}-{speed}"
-                shutil.copyfile(path+f, output_path+output_name)
+                    # Copy file into sorted folder with new name
+                    output_name = f"{trial}-{texture}-{speed}.pickle"
+                    shutil.copyfile(path+f, output_path+output_name)
 
     print("Copied dataset")
 
@@ -64,7 +82,7 @@ def main():
             "Num Speeds": num_speeds,
             "Textures": textures,
             "Speeds": speeds,
-            "Format": "trial-texture-speed",
+            "Depth": set_depth,
         },
         orient="index",
     )
