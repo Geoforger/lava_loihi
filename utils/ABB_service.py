ae0394ba98b612557d13a7794768ddb104831171
Geoforger <brayshaw.george@gmail.com>
2024-08-16 14:02:24 +0100

    Moved and finished pyro server for arm

--- utils/ABB_service.py
+++ utils/ABB_service.py
@@ -1,16 +1,21 @@
 from argparse import ArgumentParser
 from Pyro5.api import expose, Daemon, locate_ns, oneway
-from core.sensor.tactile_sensor_neuro import NeuroTac
 import subprocess
 import time
 
 from cri.robot import SyncRobot, AsyncRobot
 from cri.controller import ABBController
 
+import logging
+logging.basicConfig()  # or your own sophisticated setup
+logging.getLogger("Pyro5").setLevel(logging.DEBUG)
+logging.getLogger("Pyro5.core").setLevel(logging.DEBUG)
+
+
 @expose
 class ABBService(object):
     def __init__(self, ip="192.168.125.1") -> None:
-        self.robot = AsyncRobot(SyncRobot(ABBController(ip)))
+        self.controller = AsyncRobot(SyncRobot(ABBController(ip)))
         print(f"Connected to ABB")
 
     def __enter__(self):
@@ -20,96 +25,96 @@ class ABBService(object):
         self.close()
 
     def close(self):
-        self.robot.close()
+        self.controller.close()
 
     @property
     def info(self):
-        return self.robot.info
+        return self.controller.info
 
     @property
     def tcp(self):
-        return self.robot.tcp
+        return self.controller.tcp
 
     @tcp.setter
     def tcp(self, tcp):
-        self.robot.tcp(tcp)
+        self.controller.tcp = tcp
 
     @property
     def linear_speed(self):
-        return self.robot.linear_speed
+        return self.controller.linear_speed
 
     @linear_speed.setter
     def linear_speed(self, speed):
-        self.robot.linear_speed = speed
+        self.controller.linear_speed = speed
 
     @property
     def anglular_speed(self):
-        return self.robot.anglular_speed
+        return self.controller.anglular_speed
 
     @anglular_speed.setter
     def anglular_speed(self, speed):
-        self.robot.anglular_speed = speed
+        self.controller.anglular_speed = speed
 
     @property
     def blend_radius(self):
-        return self.robot.blend_radius
+        return self.controller.blend_radius
 
     @blend_radius.setter
     def blend_radius(self, blend_radius):
-        self.robot.blend_radius(blend_radius)
+        self.controller.blend_radius = blend_radius
 
     @property
     def joint_angles(self):
-        return self.robot.joint_angles
+        return self.controller.joint_angles
 
     @property
     def commanded_joint_angles(self):
-        return self.robot.commanded_joint_angles
+        return self.controller.commanded_joint_angles
 
     @property
     def pose(self):
-        return self.robot.pose
+        return self.controller.pose
 
     @property
     def commanded_pose(self):
-        return self.robot.command_pose
+        return self.controller.command_pose
 
     @property
     def elbow(self):
-        return self.robot.elbow
+        return self.controller.elbow
 
     @property
     def command_elbow(self):
-        return self.robot.command_elbow
+        return self.controller.command_elbow
 
     @property
     def coord_frame(self):
-        return self.robot.coord_frame
+        return self.controller.coord_frame
 
     @coord_frame.setter
     def coord_frame(self, frame):
-        self.robot.coord_frame(frame)
+        self.controller.coord_frame = frame
 
     @oneway
     def move_joints(self, joint_angles):
-        self.robot.move_joints(joint_angles)
+        self.controller.move_joints(joint_angles)
 
     def move_joints_blocking(self, joint_angles):
-        self.robot.move_joints(joint_angles)
+        self.controller.move_joints(joint_angles)
 
     @oneway
-    def move_linear(self, pose, elbow=None):
-        self.robot.move_linear(self, pose, elbow=None)
+    def move_linear(self, pose):
+        self.controller.move_linear(pose)
 
-    def move_linear_blocking(self, pose, elbow=None):
-        self.robot.move_linear(self, pose, elbow=None)
+    def move_linear_blocking(self, pose):
+        self.controller.move_linear(pose)
 
     @oneway
-    def move_circular(self, via_pose, end_pose, elbow=None):
-        self.robot.move_circular(via_pose, end_pose, elbow=None)
+    def move_circular(self, via_pose, end_pose):
+        self.controller.move_circular(via_pose, end_pose)
 
-    def move_circular_blocking(self, via_pose, end_pose, elbow=None):
-        self.robot.move_circular(via_pose, end_pose, elbow=None)
+    def move_circular_blocking(self, via_pose, end_pose):
+        self.controller.move_circular(via_pose, end_pose)
 
 
 def main():
@@ -121,7 +126,7 @@ def main():
         "-i", "--host-ip", type=str, default="127.0.0.1", help="host IP address"
     )
     parser.add_argument(
-        "-p", "--host-port", type=int, default=9092, help="host port number"
+        "-p", "--host-port", type=int, default=5000, help="host port number"
     )
     parser.add_argument(
         "-n",
