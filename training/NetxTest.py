a09693ed04235f636e2ad9f0b6085dea7d48edb4
Geoforger <brayshaw.george@gmail.com>
2024-10-07 16:37:29 +0100

    Netx test vs torch

--- training/NetxTest.py
+++ training/NetxTest.py
@@ -1,18 +1,88 @@
 import numpy as np
 import logging
 from lava.lib.dl import netx
+import lava.lib.dl.slayer as slayer
 from lava.magma.core.run_conditions import RunSteps
 from lava.magma.core.run_configs import Loihi2SimCfg
 from lava.proc.io.source import RingBuffer as InBuffer
 from lava.proc.io.sink import RingBuffer as OutBuffer
+import torch
 from torch.utils.data import DataLoader
 import time
 import pandas as pd
 
 from force_speed_dataset import ForceSpeedDatasetNetx as ForceSpeedDataset
 
+# ## Define network structure as before
+class Network(torch.nn.Module):  # Define network
+    def __init__(self, output_neurons, x_size, y_size, dropout=0.1, pruning=0.3):
+        super(Network, self).__init__()
 
-def test_with_netx(net_path, inp, label, sample_length=1000):
+        neuron_params = {
+            "threshold": 1.15,  # Previously 1.25
+            "current_decay": 0.25,  # Preivously 0.25
+            "voltage_decay": 0.03,  # Previously 0.03
+            "tau_grad": 0.03,
+            "scale_grad": 3,
+            "requires_grad": True,
+        }
+
+        self.output_neurons = int(output_neurons)
+        self.hidden_layer = 350
+
+        neuron_params_drop = {
+            **neuron_params,
+            "dropout": slayer.neuron.Dropout(p=dropout),
+        }
+
+        self.blocks = torch.nn.ModuleList(
+            [
+                slayer.block.cuba.Dense(
+                    neuron_params_drop,
+                    x_size * y_size * 1,
+                    self.hidden_layer,
+                    weight_norm=True,
+                ),
+                slayer.block.cuba.Dense(
+                    neuron_params_drop,
+                    self.hidden_layer,
+                    self.hidden_layer,
+                    weight_norm=True,
+                ),
+                slayer.block.cuba.Dense(
+                    neuron_params_drop,
+                    self.hidden_layer,
+                    self.output_neurons,
+                    weight_norm=True,
+                ),
+            ]
+        )
+    
+    def forward(self, spike):
+        for block in self.blocks:
+            spike = block(spike)
+
+        return spike
+
+    def grad_flow(self, path):
+        grad = [b.synapse.grad_norm for b in self.blocks if hasattr(b, "synapse")]
+
+        plt.figure()
+        plt.semilogy(grad)
+        plt.savefig(path + "gradFlow.png")
+        plt.close()
+
+        return grad
+    
+    def export_hdf5(self, filename):
+        # network export to hdf5 format
+        h = h5py.File(filename, 'w')
+        layer = h.create_group('layer')
+        for i, b in enumerate(self.blocks):
+            b.export_hdf5(layer.create_group(f'{i}'))
+
+
+def test_with_netx(net_path, inp, sample_length=1000):
     net = netx.hdf5.Network(
         net_config=net_path
     )
@@ -35,10 +105,28 @@ def test_with_netx(net_path, inp, label, sample_length=1000):
     output_totals = np.sum(out, axis=1)
     decision = np.argmax(output_totals)
 
-    return decision, label
+    return decision
+
+
+def test_with_torch(net_path, x_size, y_size, inp, sample_length=1000):
+    # Load network
+    net = Network(10, x_size, y_size)
+    net.load_state_dict(torch.load(net_path, map_location=torch.device("cpu")))
+    net.eval()
+
+    with torch.no_grad():
+        output = net(inp.unsqueeze(0))
+
+    output = output.numpy()
+    total_spikes = np.sum(output.squeeze(0), axis=1)
+    decision = np.argmax(total_spikes, axis=0)
+
+    return decision
 
 
 def main():
+    logging.basicConfig(filename="tests.log", level=logging.INFO)
+
     dataset_path="/media/farscope2/T7 Shield/Neuromorphic Data/George/speed_depth_preproc_downsampled/"
 
     # Load testing set
@@ -47,25 +135,37 @@ def main():
         train=False,
     )
 
-    start_time = time.time()
+    # for idx in range(len(testing_set)):
+    input, label, filename = testing_set.__getitem__(0)
 
-    for idx in range(len(testing_set)):
-        input, label, filename = testing_set.__getitem__(idx)
+    netx_pred = test_with_netx(
+        net_path="/home/farscope2/Documents/PhD/lava_loihi/networks/best_arm_network/network.net",
+        # dataset_path=dataset_path,
+        inp=input,
+        sample_length=1000
+    )
 
-        netx_pred, netx_real = test_with_netx(
-            net_path="/home/farscope2/Documents/PhD/lava_loihi/networks/best_arm_network/network.net",
-            # dataset_path=dataset_path,
-            inp=input,
-            label=label,
-            sample_length=1000
-        )
+    torch_pred = test_with_torch(
+        net_path="/home/farscope2/Documents/PhD/lava_loihi/networks/best_arm_network/network.pt",
+        x_size=67,
+        y_size=69,
+        inp=input,
+        sample_length=1000,
+    )
 
-        torch_pred, torch_real = test_with_torch(
-            net_path="/home/farscope2/Documents/PhD/lava_loihi/networks/best_arm_network/network.pt",
-            inp=input,
-            label=label,
-            sample_length=1000,
-        )
+    print(f"Label: {label} \nTorch: {torch_pred} \nNetx: {netx_pred} \n\n")
+    logging.info(filename)
+
+    output_dict = {
+        "Label": label,
+        "Torch": torch_pred,
+        "Netx": netx_pred
+    }
+
+    output_frame = pd.DataFrame(
+        data=output_dict, columns=list(output_dict.keys()), index=[0]
+    )
+    output_frame.to_csv("test_outputs.csv", mode="a", header=False)
 
 
 if __name__ == "__main__":
--- training/force_speed_dataset.py
+++ training/force_speed_dataset.py
@@ -7,10 +7,12 @@ import os
 import sys
 import pandas as pd
 from ast import literal_eval
+import re
 
 sys.path.append("..")
 from utils.utils import nums_from_string
 from utils.data_processor import DataProcessor
+
 
 class ForceSpeedDataset(Dataset):
     def __init__(
@@ -63,7 +65,70 @@ class ForceSpeedDataset(Dataset):
             sampling_time=1, dim=(1, self.y_size, self.x_size, self.num_time_bins)
         )).float()
 
-        return spike.reshape(-1, self.num_time_bins), label, speed, force
+        return spike.reshape(-1, self.num_time_bins), label, speed, force, filename
+
+    # Function to find length of dataset
+    def __len__(self):
+        return len(self.samples)
+
+
+class ForceSpeedDatasetNetx(Dataset):
+    def __init__(
+        self, path, train, valid=False, texture=True, sampling_time=10
+    ) -> None:
+
+        super(ForceSpeedDatasetNetx, self).__init__()
+
+        # Load in meta file and params from preprocessing step
+        meta = pd.read_csv(f"{path}/meta.csv")
+        self.sample_length = meta["cuttoff"].iloc[0]
+        meta["output_shape"] = meta["output_shape"].apply(literal_eval)
+        self.x_size, self.y_size = meta["output_shape"].iloc[0]
+        self.sampling_time = sampling_time
+        self.num_time_bins = int(self.sample_length / self.sampling_time)
+        self.train = train
+        self.texture = texture
+
+        # Set path to whichever dataset you want
+        if self.train is True:
+            self.PATH = f"{path}/train/"
+        else:
+            self.PATH = f"{path}/test/"
+
+        if valid is True:
+            self.PATH = f"{path}/valid/"
+
+        self.samples = glob.glob(f"{self.PATH}*_on.pickle.npy")
+
+        with open("tests.log", "r") as f:
+            log_addresses = set()
+            for line in f:
+                # Use regex to extract the address string in the new format
+                match = re.search(r"INFO:root:(/.*)", line)
+                if match:
+                    address = match.group(1)
+                    log_addresses.add(address)
+
+        self.samples = [addr for addr in self.samples if addr not in log_addresses]
+        print(len(self.samples))
+
+    # Function to retrieve spike data from index
+    def __getitem__(self, index):
+        filename = self.samples[index]
+        label = nums_from_string(filename)[-2]
+
+        event = DataProcessor.load_data_np(filename)
+        event.create_events()
+
+        event = event.data
+
+        spike = torch.from_numpy(
+            event.to_tensor(
+                sampling_time=1, dim=(1, self.y_size, self.x_size, self.num_time_bins)
+            )
+        ).float()
+
+        return spike.reshape(-1, self.num_time_bins), label, filename
 
     # Function to find length of dataset
     def __len__(self):
