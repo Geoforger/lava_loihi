94afa3614600c0a323d2f5605cf6adc15f86ada8
george <brayshaw.george@gmail.com>
2024-10-15 11:30:17 +0100

    Changed simulation to run in test, benchmark and random mode

--- simulator/simulator_run.bash
+++ simulator/simulator_run.bash
@@ -1,11 +1,20 @@
 #!/bin/bash
-for l in $(seq 0 10)
+declare -a arr=("r" "t" "b")
+# Cycle through modes
+for m in "${arr[@]}"
 do
-    echo "Testing label: $l"
-    for i in $(seq 1 1000)
+    echo "Testing Mode: $m"
+    # Cycle through all textures
+    for l in $(seq 0 10)
     do
-        echo "Iteration $i - $(date)"
-        echo "$l" | python /home/george/Documents/lava_loihi/simulator/control_simulator.py
-        sleep 0.2
+        echo "Testing Label: $l"
+        # Implement all tests using these settings
+        for i in $(seq 1 200)
+        do
+            echo "Iteration $i - $(date)"
+            # Pass both mode and label as input to the python script
+            printf "$m\n$l\n" | python /home/george/Documents/lava_loihi/simulator/control_simulator.py
+            sleep 0.2
+        done
     done
 done
